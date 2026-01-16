897e45e7669a7c3fa47365b34f79b6004d667292
Alihuseyn-Ismayilov <elihuseynismayilov87@gmail.com>
2026-01-16 16:22:41 +0400

    changed backend url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = 'https://ayna-backend-n4vl.onrender.com';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
